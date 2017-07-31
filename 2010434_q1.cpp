0508d2e1bc99d6bfb9cf14ba75c76c496a76e777
prithvisbisht <prithvisbisht@gmail.com>
2017-07-31 10:52:25 +0530

    file edited according to lab format

---
+++ 2010434_q1.cpp
@@ -0,0 +1,41 @@
+/*
+Name-Prithvi Raj Singh Bisht
+Roll_no- 55
+Section-B
+*/
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int t,i;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		int compare=0;
+        cin >> n;
+        int a[n];
+        for(i=0;i<n;i++)
+        {
+            cin >> a[i];
+        }
+        int key;
+        cin >> key;
+        for(i=0;i<n;i++)
+        {
+            compare++;
+            if(a[i]==key)
+            {
+                cout<<"Present "<<endl;
+                break;
+            }
+        }
+        if(i==n)
+        {
+            cout<<" Not Present "<<endl;
+        }
+        cout<<"total comparisons-->"<<compare<<endl;
+	}
+}
---
+++ 2010434_q3.cpp
@@ -0,0 +1,54 @@
+/*
+Name-Prithvi Raj Singh Bisht
+Roll_no- 55
+Section-B
+*/
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int jsearch(int a[],int l,int k)
+{
+	int left=0;
+	int jump=sqrt(l);//this is th jump that is to be made during searching this can differ from user to user;
+	int right=jump;
+	while(a[right]<=k && right < l)
+	{
+		left=right;
+		right+=jump;
+		if(right > l-1)
+			right=l;
+	}
+	for(int i=left;i < right;i++)
+	{
+		if(a[i]==k)
+			{
+				//cout<<" Hello";
+				return i;
+			}
+	}
+	return -1;
+}
+int main()
+{
+	int t,x;
+	cin>>t;
+	while(t--)
+	{
+		int n,i;
+		cin>>n;
+		int a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		int key;
+		cin>>key;
+		x=jsearch(a,n,key);
+		if(x == -1)
+			cout<<"key not found"<<endl;
+		else
+			cout<<"key found at index "<<x<<endl;
+	}
+}
